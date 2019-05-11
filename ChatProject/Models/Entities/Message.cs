836bdd19ce731e73ee0906a7b4dbe8887d287eb5
barakoo6am <barakoo6am@gmail.com>
2019-05-11 09:43:45 -0700

    added room & message models

--- ChatProject/Models/Entities/Message.cs
+++ ChatProject/Models/Entities/Message.cs
@@ -1,7 +1,12 @@
+using System;
+
 namespace ChatProject.Models.Entities
 {
     public class Message
     {
-        
+        public int ID { get; set; }
+        public virtual User User { get; set; }
+        public string Text { get; set; }
+        public DateTime DateTime { get; set; }
     }
 }
--- ChatProject/Models/Entities/Room.cs
+++ ChatProject/Models/Entities/Room.cs
@@ -1,7 +1,15 @@
+using System;
+using System.Collections.Generic;
+
 namespace ChatProject.Models.Entities
 {
     public class Room
     {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public Boolean Private { get; set; }
         
+        public virtual ICollection<User> Users { get; set; }
+        public virtual ICollection<Message> Messages { get; set; }
     }
 }
