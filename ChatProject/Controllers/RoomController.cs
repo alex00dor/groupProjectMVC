390b55edd0e09905582599b11a546c925cf9fd87
barakoo6am <barakoo6am@gmail.com>
2019-05-27 10:50:55 -0700

    paging redesign

--- ChatProject/Controllers/RoomController.cs
+++ ChatProject/Controllers/RoomController.cs
@@ -16,7 +16,7 @@ namespace ChatProject.Controllers
     {
         private IRoomRepository _repository;
         private UserManager<User> _userManager;
-        private static int PAGE_SIZE = 10;
+        private static int PAGE_SIZE = 5;
 
         public RoomController(IRoomRepository repository, UserManager<User> userManager)
         {
--- ChatProject/Infrastructure/PageLinkTagHelper.cs
+++ ChatProject/Infrastructure/PageLinkTagHelper.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Razor.TagHelpers;
 
 namespace ChatProject.Infrastructure
 {
-    [HtmlTargetElement("div", Attributes = "page-model")]
+    [HtmlTargetElement("ul", Attributes = "page-model")]
     public class PageLinkTagHelper : TagHelper
     {
         private IUrlHelperFactory urlHelperFactory;
@@ -17,36 +17,68 @@ namespace ChatProject.Infrastructure
             urlHelperFactory = helperFactory;
         }
 
-        [ViewContext] 
-        [HtmlAttributeNotBound] 
-        public ViewContext ViewContext { get; set; }
+        [ViewContext] [HtmlAttributeNotBound] public ViewContext ViewContext { get; set; }
         public PagingInfo PageModel { get; set; }
+        public bool PagePreviousNext { get; set; } = false;
         public string PageAction { get; set; }
-        public bool PageClassesEnabled { get; set; } = false;
-        public string PageClass { get; set; }
+        public string PageClassLink { get; set; }
         public string PageClassNormal { get; set; }
         public string PageClassSelected { get; set; }
+        public string PageClassDisabled { get; set; }
 
         public override void Process(TagHelperContext context,
             TagHelperOutput output)
         {
             IUrlHelper urlHelper = urlHelperFactory.GetUrlHelper(ViewContext);
-            TagBuilder result = new TagBuilder("div");
+            TagBuilder result = new TagBuilder("ul");
+            if (PagePreviousNext)
+            {
+                TagBuilder li = new TagBuilder("li");
+                int prev = PageModel.CurrentPage - 1;
+                li.AddCssClass(prev > 0 ? PageClassNormal : PageClassDisabled);
+                TagBuilder tag = new TagBuilder("span");
+                if (prev > 0)
+                {
+                    tag = new TagBuilder("a");
+                    tag.Attributes["href"] = urlHelper.Action(PageAction, new {page = prev});
+                }
+                tag.AddCssClass(PageClassLink);
+                tag.InnerHtml.Append("Previous");
+                li.InnerHtml.AppendHtml(tag);
+                result.InnerHtml.AppendHtml(li);
+
+            }
             for (int i = 1; i <= PageModel.TotalPages; i++)
             {
-                TagBuilder tag = new TagBuilder("a");
-                tag.Attributes["href"] = urlHelper.Action(PageAction,
-                    new {page = i});
-                if (PageClassesEnabled)
+                TagBuilder li = new TagBuilder("li");
+                li.AddCssClass(i == PageModel.CurrentPage? PageClassSelected : PageClassNormal);
+                TagBuilder tag = new TagBuilder("span");
+                if (i != PageModel.CurrentPage)
                 {
-                    tag.AddCssClass(PageClass);
-                    tag.AddCssClass(i == PageModel.CurrentPage
-                        ? PageClassSelected
-                        : PageClassNormal);
+                    tag = new TagBuilder("a");
+                    tag.Attributes["href"] =  urlHelper.Action(PageAction, new {page = i});
                 }
-
+                tag.AddCssClass(PageClassLink);
                 tag.InnerHtml.Append(i.ToString());
-                result.InnerHtml.AppendHtml(tag);
+                li.InnerHtml.AppendHtml(tag);
+                result.InnerHtml.AppendHtml(li);
+            }
+            
+            if (PagePreviousNext)
+            {
+                TagBuilder li = new TagBuilder("li");
+                int next = PageModel.CurrentPage + 1;
+                li.AddCssClass(next <= PageModel.TotalPages ? PageClassNormal : PageClassDisabled);
+                TagBuilder tag = new TagBuilder("span");
+                if (next <= PageModel.TotalPages)
+                {
+                    tag = new TagBuilder("a");
+                    tag.Attributes["href"] = urlHelper.Action(PageAction, new {page = next});
+                }
+                tag.AddCssClass(PageClassLink);
+                tag.InnerHtml.Append("Next");
+                li.InnerHtml.AppendHtml(tag);
+                result.InnerHtml.AppendHtml(li);
             }
 
             output.Content.AppendHtml(result.InnerHtml);
