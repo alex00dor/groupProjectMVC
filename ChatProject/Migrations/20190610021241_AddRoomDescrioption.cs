5736d0f18e26d47d4b2cbee4a162957b8e8cc19b
barakoo6am <barakoo6am@gmail.com>
2019-06-12 10:02:54 -0700

    add migration

---
+++ ChatProject/Migrations/20190610021241_AddRoomDescrioption.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ChatProject.Migrations
+{
+    public partial class AddRoomDescrioption : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Rooms",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Rooms");
+        }
+    }
+}
